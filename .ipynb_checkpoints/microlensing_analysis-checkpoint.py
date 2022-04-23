c912898661fe8496d89c99e360cd558d5cc53185
Paul Zürn <paul.zuern1@web.de>
2022-04-23 15:13:35 +0200

    Added logic to automatically determine the run path

--- .ipynb_checkpoints/microlensing_analysis-checkpoint.py
+++ .ipynb_checkpoints/microlensing_analysis-checkpoint.py
@@ -1,11 +1,12 @@
+import os
+import h5py
 import numpy as np
 from matplotlib import pyplot as plt
-import os
 
 # Defining the paths to the simulation results
 
 run_path = 'H:/Bachelorarbeit/Daten/runs/'
-run_path = '/work/Tit6/paul.zuern/data/runs/'
+if not os.path.exists(run_path): run_path = '/work/Tit6/paul.zuern/data/runs/'
 
 # Reading the data
 
