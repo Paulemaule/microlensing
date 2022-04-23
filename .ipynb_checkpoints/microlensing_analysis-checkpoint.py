99143cacbdc76bdecdb2856f30bb2c1f4b005e5e
Paul Zürn <paul.zuern1@web.de>
2022-04-23 15:02:50 +0200

    Test commit

--- .ipynb_checkpoints/microlensing_analysis-checkpoint.py
+++ .ipynb_checkpoints/microlensing_analysis-checkpoint.py
@@ -5,11 +5,12 @@ import os
 # Defining the paths to the simulation results
 
 run_path = 'H:/Bachelorarbeit/Daten/runs/'
-#run_path = '/work/Tit6/paul.zuern/data/runs/'
+run_path = '/work/Tit6/paul.zuern/data/runs/'
 
 # Reading the data
 
 for run in sorted(os.listdir(runs_path), key = lambda x: int(x[4:])):
     print(run)
+    print('TEST')
     
 print('END')
