a722abdcd395b086f9c290c826dad0b81815e756
Paul Zürn <paul.zuern1@web.de>
2022-04-23 13:51:14 +0200

    Test for the reading of the run folder

--- microlensing_analysis.py
+++ microlensing_analysis.py
@@ -1,2 +1,15 @@
 import numpy as np
 from matplotlib import pyplot as plt
+
+
+# Defining the paths to the simulation results
+
+run_path = 'H:/Bachelorarbeit/Daten/runs/'
+#run_path = '/work/Tit6/paul.zuern/data/runs/'
+
+# Reading the data
+
+for run in sorted(os.listdir(runs_path), key = lambda x: int(x[4:])):
+    print(run)
+    
+print('END')
