bcf21eaa0cc91f29d8c5f807f4879c5414d5c891
Paul Zürn <paul.zuern1@web.de>
2022-04-27 22:32:11 +0200

    Added microlensing event search and event saving into dictionary

--- microlensing_analysis.py
+++ microlensing_analysis.py
@@ -20,18 +20,21 @@ for run in sorted(os.listdir(runs_path), key = lambda x: int(x[4:])):
         TSCALE  = f[list(f.keys())[-1]]['000 Scalars'][10]
         N = f[list(f.keys())[-1]]['000 Scalars'][4]
         
-    print(f' General Properties: N = {int(N)}, RBAR = {RBAR:.3f}, TSCALE = {TSCALE:.3f}')
+    print(f'  General Properties: N = {int(N)}, RBAR = {RBAR:.3f}, TSCALE = {TSCALE:.3f}')
+    
+    # The dictionary that will store the found microlensing events
+    events = {'time': [], 'id_ffp': [], 'id_star': [], 'x1_ffp': [], 'x2_ffp': [], 'x3_ffp': [], 'x1_star':[], 'x2_star':[], 'x3_star':[]}
     
     for snap in sorted(filter(lambda x: ('snap' in x), os.listdir(runs_path + run)), key = lambda x: int(x.split('.')[1][3:])):
         if int(snap.split('.')[1][3:]) == 20: break
         
-        print('  On Snapshot ' + snap.split('.')[1][3:])
+        print('    On Snapshot ' + snap.split('.')[1][3:])
         
         with h5py.File(run_path + snap) as f:
             for step in sorted(f.keys(), key = lambda x: int(x[5:])):
                 # Exclude the timesteps with non integer times
                 if f[step]['000 Scalars'][0] % 1 != 0: continue
-                print('   At time {:.0f}'.format(f[step]['000 Scalars'][0]))
+                print('      At time {:.0f}'.format(f[step]['000 Scalars'][0]))
                 
                 # Read the date
                 i = np.array(f[step]['032 Name'])
@@ -67,10 +70,26 @@ for run in sorted(os.listdir(runs_path), key = lambda x: int(x[4:])):
                     # Calculate the disance in the observation plane
                     distances = np.linalg.norm(x[axes][:,stars][:,neighbours] - x[axes,ffp_num][:,np.newaxis], axis = 0)
                     
-                    # 
+                    # Find the neighbour stars that will be lensed by the free floating planet and saving them as events
+                    lensed = distances < 0.01
                     
-                    break
-                
+                    for star_num in np.arange(len(m))[stars][neighbours][lensed]:
+                        events['time'].append(f[step]['000 Scalars'][0])
+                        events['id_ffp'].append(i[ffp_num])
+                        events['id_star'].append(i[star_num])
+                        events['x1_ffp'].append(x[0,ffp_num])
+                        events['x2_ffp'].append(x[1,ffp_num])
+                        events['x3_ffp'].append(x[2,ffp_num])
+                        events['x1_star'].append(x[0,star_num])
+                        events['x2_star'].append(x[1,star_num])
+                        events['x3_star'].append(x[2,star_num])
+                        
+                    #break
+        
+    print(('   '.join(['{:13s}',] * len(events.keys())).format(*events.keys())))
+    for e in np.arange(len(events['time'])):
+        print(('   '.join(['{:13s}',] * len(events.keys())).format(*['{:.6g}'.format(events[key][e]) for key in events.keys()])))
+    
     break
     
 print('END')
