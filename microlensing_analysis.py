487ac97c4a79e103582b2c4754e012cf72fba14f
Paul Zürn <paul.zuern1@web.de>
2022-05-01 00:09:06 +0200

    Properly implemented KDTree, started the better console output and timing functions

--- microlensing_analysis.py
+++ microlensing_analysis.py
@@ -1,8 +1,8 @@
 import os
+from time import time_ns
 import h5py
 import numpy as np
 from scipy.spatial import KDTree
-from matplotlib import pyplot as plt
 
 # Defining the paths to the simulation results
 
@@ -27,15 +27,15 @@ for run in sorted(os.listdir(runs_path), key = lambda x: int(x[4:])):
     events = {'time': [], 'id_ffp': [], 'id_star': [], 'x1_ffp': [], 'x2_ffp': [], 'x3_ffp': [], 'x1_star':[], 'x2_star':[], 'x3_star':[]}
     
     for snap in sorted(filter(lambda x: ('snap' in x), os.listdir(runs_path + run)), key = lambda x: int(x.split('.')[1][3:])):
-        if int(snap.split('.')[1][3:]) == 20: break
-        
-        print('    On Snapshot ' + snap.split('.')[1][3:])
+        #if int(snap.split('.')[1][3:]) == 20: break
+        start_time = time_ns()
         
         with h5py.File(run_path + snap) as f:
             for step in sorted(f.keys(), key = lambda x: int(x[5:])):
                 # Exclude the timesteps with non integer times
                 if f[step]['000 Scalars'][0] % 1 != 0: continue
-                print('      At time {:.0f}'.format(f[step]['000 Scalars'][0]))
+                
+                print('    On Snapshot {} - at time {:.0f}'.format(snap.split('.')[1][3:], f[step]['000 Scalars'][0]))
                 
                 # Read the date
                 i = np.array(f[step]['032 Name'])
@@ -54,24 +54,13 @@ for run in sorted(os.listdir(runs_path), key = lambda x: int(x[4:])):
                 axes = [0,1]
                 
                 # Construct the KD-Tree
-                tree = KDTree(x[axes,:][:,stars].T, leafsize=40, compact_nodes=True, copy_data=False, balanced_tree=True,)
-                #neighbours = tree.query_ball_point(x[axes,:][:,ffps].T, 0.01)
-                neighbours = tree.query_ball_point(x[axes,:][:,ffps][:,[0,1]].T, 0.01)
-                print(neighbours)
+                leafsize = 20
+                tree_stars = KDTree(x[axes,:][:,stars].T, leafsize = leafsize)
+                tree_ffps = KDTree(x[axes,:][:,ffps].T, leafsize = leafsize)
+                neighbours = tree_ffps.query_ball_tree(tree_stars, 0.01)
                 
-                for ffp in neighbours:
-                    print(ffp)
-                
-                #    for star_num in np.arange(len(m))[stars][neighbours][lensed]:
-                #        events['time'].append(f[step]['000 Scalars'][0])
-                #        events['id_ffp'].append(i[ffp_num])
-                #        events['id_star'].append(i[star_num])
-                #        events['x1_ffp'].append(x[0,ffp_num])
-                #        events['x2_ffp'].append(x[1,ffp_num])
-                #        events['x3_ffp'].append(x[2,ffp_num])
-                #        events['x1_star'].append(x[0,star_num])
-                #        events['x2_star'].append(x[1,star_num])
-                #        events['x3_star'].append(x[2,star_num])
+                #print(x[axes,:][:,ffps][:,:5])
+                #print([item for sublist in neighbours[:5] for item in sublist])
         
     print(('   '.join(['{:13s}',] * len(events.keys())).format(*events.keys())))
     for e in np.arange(len(events['time'])):
